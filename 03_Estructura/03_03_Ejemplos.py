f0e9586f727550129f246a6bc325c6d5b67c90a5
Mario Zamora <mortasoft@gmail.com>
2021-01-04 20:48:59 -0600

    04/01/2021

---
+++ 03_Estructura/03_03_Ejemplos.py
@@ -0,0 +1,9 @@
+import modulo3.saludos as ss
+
+ss.buenos_dias()
+print(ss.CONSTANTE_SALUDO)
+
+from modulo3.saludos import buenas_tardes
+buenas_tardes()
+
+
---
+++ 03_Estructura/modulo3/saludos.py
@@ -0,0 +1,7 @@
+CONSTANTE_SALUDO="Hola!"
+
+def buenos_dias():
+    print("Buenos dias!")
+    
+def buenas_tardes():
+    print("Buenas tardes!")
